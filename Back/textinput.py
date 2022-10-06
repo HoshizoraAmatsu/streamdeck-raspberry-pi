481a9d4a0056aeda5c22dc431c8df2cb1d0c4884
HoshizoraAmatsu <guishikaw@gmail.com>
2022-10-06 16:47:25 -0300

    p

--- Back/textinput.py
+++ Back/textinput.py
@@ -1,12 +1,12 @@
 import string
 from pynput.keyboard import Controller
 import sys
-import time
+
 
 keyboard = Controller()
 
 textinput = sys.stdin.readline()
 
-time.sleep(2)
+
 
 keyboard.type(textinput)
